fbffd6acc223e96ea9f0f16f9b0b109e66a4301d
Iryna <iryna.burianovata@gmail.com>
2024-05-10 16:25:14 +0300

    Set up Three.js scene

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -17,6 +17,7 @@ export default function Shapes() {
         camera={{ position: [0, 0, 25], fov: 30, near: 1, far: 40 }}
       >
         <Suspense fallback={null}>
+          <Geometries />
           <ContactShadows
             position={[0, -3.5, 0]}
             opacity={0.65}
@@ -28,5 +29,114 @@ export default function Shapes() {
         </Suspense>
       </Canvas>
     </div>
+  );
+}
+
+function Geometries() {
+  const geometries = [
+    {
+      position: [0, 0, 0],
+      r: 0.3,
+      geometry: new THREE.IcosahedronGeometry(3),
+    },
+  ];
+
+  const materials = [
+    new THREE.MeshNormalMaterial(),
+    new THREE.MeshStandardMaterial({ color: 0x2ecc71, roughness: 0 }),
+    new THREE.MeshStandardMaterial({
+      color: 0xeb4d4b,
+      roughness: 0.1,
+      metalness: 0.5,
+    }),
+    new THREE.MeshStandardMaterial({
+      color: 0xc7ecee,
+      roughness: 0.1,
+      metalness: 0.5,
+    }),
+    new THREE.MeshStandardMaterial({
+      color: 0x7ed6df,
+      roughness: 0.1,
+      metalness: 0.5,
+    }),
+    new THREE.MeshStandardMaterial({
+      color: 0xf6e58d,
+      roughness: 0.1,
+      metalness: 0.5,
+    }),
+  ];
+
+  return geometries.map(({ position, r, geometry }) => (
+    <Geometry
+      key={JSON.stringify(position)}
+      position={position.map((p) => p * 2)}
+      geometry={geometry}
+      materials={materials}
+      r={r}
+    />
+  ));
+}
+
+function Geometry({ r, position, geometry, materials }) {
+  const meshRef = useRef();
+  const [visible, setVisible] = useState(false);
+
+  const startingMaterial = getRandomMaterial();
+
+  function getRandomMaterial() {
+    return gsap.utils.random(materials);
+  }
+
+  function handleClick(e) {
+    const mesh = e.object;
+
+    gsap.to(mesh.rotation, {
+      x: `+=${gsap.utils.random(0, 2)}`,
+      y: `+=${gsap.utils.random(0, 2)}`,
+      z: `+=${gsap.utils.random(0, 2)}`,
+      duration: 1.3,
+      ease: "elastic.out(1,0.3)",
+    });
+
+    mesh.material = getRandomMaterial();
+  }
+
+  const handlePointerOver = () => {
+    document.body.style.cursor = "pointer";
+  };
+
+  const handlePointerOut = () => {
+    document.body.style.cursor = "default";
+  };
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      setVisible(true);
+      gsap.from(meshRef.current.scale, {
+        x: 0,
+        y: 0,
+        z: 0,
+        duration: 1,
+        ease: "elastic.out(1, 0.3)",
+        delay: 0.3,
+      });
+    });
+
+    return () => ctx.revert();
+  }, []);
+
+  return (
+    <group position={position} ref={meshRef}>
+      <Float speed={5 * r} rotationIntensity={6 * r} floatIntensity={5 * r}>
+        <mesh
+          geometry={geometry}
+          onClick={handleClick}
+          onPointerOver={handlePointerOver}
+          onPointerOut={handlePointerOut}
+          visible={visible}
+          material={startingMaterial}
+        ></mesh>
+      </Float>
+    </group>
   );
 }
