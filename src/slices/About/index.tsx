43e97ee54fd5758a0088693ac76ce8e30eaa3af8
Iryna <iryna.burianovata@gmail.com>
2024-05-13 12:53:39 +0300

    Add About section

--- src/slices/About/index.tsx
+++ src/slices/About/index.tsx
@@ -1,5 +1,8 @@
+import Button from "@/components/Button";
+import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
  * Props for `About`.
@@ -14,8 +17,26 @@ const About = ({ slice }: AboutProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="max-w-7xl mx-auto px-4 md:px-10 h-screen"
     >
-      Placeholder component for about (variation: {slice.variation}) Slices
+      <div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr,1fr]">
+        <Heading as="h1" size="xl" className="col-start-1">
+          {slice.primary.title}
+        </Heading>
+
+        <div className="prose prose-xl prose-slate prose-invert col-start-1">
+          <PrismicRichText field={slice.primary.text} />
+        </div>
+        <Button
+          linkField={slice.primary.button_link}
+          label={slice.primary.button_text}
+        />
+
+        <PrismicNextImage
+          field={slice.primary.image}
+          className="row-start-1 max-w-sm md:col-start-2 md:row-end-3"
+        />
+      </div>
     </section>
   );
 };
